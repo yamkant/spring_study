6c37965b4be53846edfcf24779c437608f183d15
yamkant <dev.yamkim@gmail.com>
2023-02-20 22:38:01 +0900

    feat: #1 [spring_mvc_1] @ModelAttribute를 활용한 HTTP 요청 데이터 받는 예제 추가

--- spring_mvc_1/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ spring_mvc_1/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,8 +1,12 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,5 +23,32 @@ public class RequestParamController {
         log.info("username={}, age={}", username, age);
 
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+//    public String modelAttributeV1(@RequestParam String username, @RequestParam int age) {
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+
+        // NOTE: @ModelAttribute의 역할: getUsername -> username property 파싱 / getAge -> age property 파싱
+        // NOTE: BindingException: 파라미터의 타입이 맞지 않는 경우 발생하는 예외입니다.
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}", helloData);
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        // NOTE: String / int / Integer와 같은 단순한 타입을 사용한다면, @RequestParam을 사용하고
+        //       내가 만드는 클래스같은 경우에는, ModelAttribute를 자동으로 사용합니다.
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData={}", helloData);
+
+        return "ok";
     }
 }
