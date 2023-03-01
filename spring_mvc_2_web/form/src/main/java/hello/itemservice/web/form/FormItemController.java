ab74948fff320473f084866e6ac34daabb214ad5
yamkant <dev.yamkim@gmail.com>
2023-03-01 21:26:20 +0900

    feat: #7 [spring_mvc_2_web] 단일 체크박스 처리 방식 구현 및 발생할 수 있는 오류 처리

--- spring_mvc_2_web/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ spring_mvc_2_web/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
@@ -39,6 +41,11 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+        // NOTE: 체크박스가 체크된 경우는 open=on와 _open=on 둘다 볼 수 있고 true로 인식,
+        //       체크박스 체크되지 않은 경우는 _open=on 정보만 받을 수 있으므로 false로 인식합니다.
+        log.info("item.open={}", item.getOpen());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
