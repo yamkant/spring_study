b09441ff8ba64bcff059926cf9c37bbf4b5e598c
yamkant <dev.yamkim@gmail.com>
2023-03-04 22:35:35 +0900

    feat: #7 [spring_mvc_2_web] 상품상세 및 수정에 멀티 체크박스 기능 전반적으로 추가 /  @ModelAttribute 사용 방법 추가

--- spring_mvc_2_web/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ spring_mvc_2_web/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,6 +20,19 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    // NOTE: 이 컨트롤러를 호출할 때, 항상 regions 모델에 자동으로 addAttribute해서 담깁니다.
+    // NOTE: 등록 폼 / 수정 폼 / 조회 폼 모두에서 사용하는 모델이므로
+    //       각 컨트롤러가 호출될 때 다른 작업을 하지 않더라도 모델에 regions 값들이 등록되어 있습니다.
+    // TODO: 성능 최적화를 위해서는 STATIC으로 사용하여 호출시마다 생성되지 않도록 수정 가능합니다.
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -45,6 +60,7 @@ public class FormItemController {
         // NOTE: 체크박스가 체크된 경우는 open=on와 _open=on 둘다 볼 수 있고 true로 인식,
         //       체크박스 체크되지 않은 경우는 _open=on 정보만 받을 수 있으므로 false로 인식합니다.
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
