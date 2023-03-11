99e3a605cacd74e6312b6d68683d688a16212158
yamkant <dev.yamkim@gmail.com>
2023-03-11 11:10:59 +0900

    feat: #11 [spring_mvc_2_web] [Validation] 필드에러가 발생하는 경우, 비어있는 필드에 입력해두었던 값 내려받도록 추가

--- spring_mvc_2_web/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ spring_mvc_2_web/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -46,8 +46,7 @@ public class ValidationItemControllerV2 {
     }
 
     // NOTE: 기존에 HashMap으로 관리하던 에러들을 BindingResult로 관리합니다.
-    @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         // 검증 오류 결과 보관 객체 생성
         Map<String, String> errors = new HashMap<>();
@@ -66,6 +65,54 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item", "가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값: = " + resultPrice));
+            }
+        }
+        // 검증에 실패하면 다시 입력 폼으로 돌아가도록 설정합니다.
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", errors);
+            model.addAttribute("errors", errors);
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    // NOTE: spring으로 값이 넘어올 때, item 객체에 넣기 전, BindingResult는 입력받은 값을 넣어둡니다.
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        // 검증 오류 결과 보관 객체 생성
+        Map<String, String> errors = new HashMap<>();
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            // 실패하더라도 data 자체는 잘 들어오게 됩니다. 하지만 잘못된 데이터 전송시 인풋 필드에서 값이 지워지게 되는데
+            // 이를 처리하기 위해 서버에서 받은 값을 itemName의 item.getItemName에 넣어줍니다.
+            bindingResult.addError(new FieldError("item", "itemName",
+                    item.getItemName(), false, null, null,
+                    "상품 이름은 필수입니다."));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price",
+                    item.getPrice(), false, null, null,
+                    "가격은 1,000 ~ 1,000,000까지 허용합니다."));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity",
+                    item.getQuantity(), false, null, null,
+                    "수량은 최대 9,999까지 허용합니다."));
+        }
+
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                // ObjectError는 필드로 값이 넘어오는게 아닌, 여러 필드의 값을 조합하는 역할을 하기 때문에, 필드 에러와 다릅니다.
+                bindingResult.addError(new ObjectError("item",
+                        null, null,
+                        "가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값: = " + resultPrice));
             }
         }
         // 검증에 실패하면 다시 입력 폼으로 돌아가도록 설정합니다.
