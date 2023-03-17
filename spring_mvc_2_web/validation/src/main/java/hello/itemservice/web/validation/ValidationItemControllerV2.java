dae2383a8d98008f21dea859a0ed400e265d66de
yamkant <dev.yamkim@gmail.com>
2023-03-17 12:12:01 +0900

    feat: #11 [spring_mvc_2_web] [Validation] 검증 관련 부분 Validator Component로 이전

--- spring_mvc_2_web/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ spring_mvc_2_web/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -25,6 +25,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -178,7 +179,7 @@ public class ValidationItemControllerV2 {
     }
 
     // NOTE: addError를 rejectValue로 변경하여 처리합니다. bindingResult가 ObjectName, target을 반환하는 것에 대한 규칙을 적용합니다.
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         log.info("objectName={}", bindingResult.getObjectName());
@@ -208,6 +209,27 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000) {
                 bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
+
+        // 검증 로직
+        itemValidator.validate(item, bindingResult);
+
+        // 검증에 실패하면 다시 입력 폼으로 돌아가도록 설정합니다.
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
         }
 
         // 성공 로직
