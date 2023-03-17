1fe5a6f4540f524207b811e46dd742a0da754edd
yamkant <dev.yamkim@gmail.com>
2023-03-17 23:08:18 +0900

    feat: #14 [spring_mvc_2_web] [Bean Validation] @Data의 Annotation들에 groups를 사용하여 상황마다 다른 기능을 수행하도록 수정

--- spring_mvc_2_web/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ spring_mvc_2_web/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -13,15 +13,22 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원을 넘게 입력해주세요.")
 public class Item {
 
+    // 수정시에 요구사항이 아래와 같이 변경된다면?
+    // 1. id는 수정시 null이면 안됩니다. -> id 필드에 @NotNull 추가
+    // 2. 수량은 등록할 때 제한이 있었지만, 수정할 떄는 제한이 없습니다. -> quantity 필드에 @Max 제거
+    // 하지만, 위의 두 방식으로 수정하면 등록 상황에서는 의도대로 동작하지 않는 side-effect가 생깁니다.
+    // 이를 해결하기 위해 groups를 사용하여 각 경우에 해당하는 annotation을 사용합니다.
+
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
     @Range(min = 1000, max = 1000000)
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
--- spring_mvc_2_web/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ spring_mvc_2_web/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -2,6 +2,8 @@ package hello.itemservice.web.validation;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -53,8 +55,32 @@ public class ValidationItemControllerV3 {
     // 1. ModelAttribute가 검증 애노테이션을 확인합니다.
     // 2. 성공하면 넘어가지만 Data Binding이 실패하면, typeMissmatch 등 오류를 FieldError에 추가합니다.
     //    바인딩에 성공한 필드만 BeanValidation에서 검증하므로, 값이 정상적으로 들어온 이후 검증합니다.
+    // @PostMapping("/add")
+    public String addItemV1(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        // NOTE: Object 에러가 발생할 경우에 대한 오류 메시지 처리
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로 돌아가도록 설정합니다.
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    // NOTE: SaveCheck에 대한 그룹만 사용하도록 Validated 수정
     @PostMapping("/add")
-    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String addItemV2(@Validated(SaveCheck.class) @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         // NOTE: Object 에러가 발생할 경우에 대한 오류 메시지 처리
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
@@ -83,9 +109,8 @@ public class ValidationItemControllerV3 {
         return "validation/v3/editForm";
     }
 
-    @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
-        itemRepository.update(itemId, item);
+    // @PostMapping("/{itemId}/edit")
+    public String editV1(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
 
         if (item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
@@ -100,6 +125,28 @@ public class ValidationItemControllerV3 {
             return "validation/v3/editForm";
         }
 
+        itemRepository.update(itemId, item);
+        return "redirect:/validation/v3/items/{itemId}";
+    }
+
+    // NOTE: SaveCheck에 대한 그룹만 사용하도록 Validated 수정
+    @PostMapping("/{itemId}/edit")
+    public String editV2(@PathVariable Long itemId, @Validated(UpdateCheck.class) @ModelAttribute Item item, BindingResult bindingResult) {
+
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        // 검증에 실패하면 다시 입력 폼으로 돌아가도록 설정합니다.
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
+        itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
 
