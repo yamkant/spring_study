2053f1f745c684bdcd99e8bddc9dda4be51222a7
yamkant <dev.yamkim@gmail.com>
2023-03-18 14:06:41 +0900

    feat: #14 [spring_mvc_2_web] [로그인] 쿠키를 이용한 로그인 및 로그아웃 처리기능 추가

--- spring_mvc_2_web/login/src/main/java/hello/login/web/HomeController.java
+++ spring_mvc_2_web/login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,41 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    // NOTE: 로그인을 하지 않은 사용자도 들어올 수 있어야하므로, required = false로 처리합니다.
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        if (memberId == null) {
+            return "home";
+        }
+
+        // NOTE: 쿠키를 통해 멤버를 찾습니다. 없으면 home으로, 있다면 loginHome으로 이동합니다.
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
