ef042d735079d848206bdf47cf89b27dccd2421f
yamkant <dev.yamkim@gmail.com>
2023-03-18 12:55:32 +0900

    feat: #14 [spring_mvc_2_web] [로그인] 로그인 시, 멤버 비교 및 없는 경우 오류 처리

---
+++ spring_mvc_2_web/login/src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,33 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * @return null이면 로그인 실패
+     */
+    public Member login(String loginId, String password) {
+//        Optional<Member> findMemberOptional = memberRepository.findByLoginId(loginId);
+//        Member member = findMemberOptional.get();
+//        if (member.getPassword().equals(password)) {
+//            return member;
+//        } else {
+//            return null;
+//        }
+
+        // NOTE: 위 로직을 다음과 같이 Optional Stream으로 사용할 수 있습니다.
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
