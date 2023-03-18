0bf0158165b496d9ad62e2d37e63b407239cdec9
yamkant <dev.yamkim@gmail.com>
2023-03-18 22:58:15 +0900

    feat: #14 [spring_mvc_2_web] [로그인] Servlet에서 기본적으로 제공하는 쿠키 세션 방식으로 사용해보기

--- spring_mvc_2_web/login/src/main/java/hello/login/web/HomeController.java
+++ spring_mvc_2_web/login/src/main/java/hello/login/web/HomeController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -43,7 +45,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         // 세션 관리자에 저장된 회원 정보 조회
@@ -54,6 +56,43 @@ public class HomeController {
             return "home";
         }
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+//    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
+
+        // NOTE: 세션을 생성할 이유가 없습니다.
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // NOTE: 세션에 회원 데이터가 없으면 home으로
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // NOTE: 세션이 유지되면 로그인으로 이동합니다.
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    // NOTE: Spring에서 기본적으로 제공하는 애노테이션을 통해 세션 확인 프로세스를 처리합니다.
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model) {
+
+        // NOTE: 세션에 회원 데이터가 없으면 home으로
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // NOTE: 세션이 유지되면 로그인으로 이동합니다.
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
--- spring_mvc_2_web/login/src/main/java/hello/login/web/login/LoginController.java
+++ spring_mvc_2_web/login/src/main/java/hello/login/web/login/LoginController.java
@@ -3,6 +3,7 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
+import hello.login.web.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,7 @@ import javax.naming.Binding;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Slf4j
@@ -53,7 +55,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//   @PostMapping("/login")
     public String loginV2(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -75,15 +77,51 @@ public class LoginController {
         return "redirect:/";
     }
 
+    @PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        // NOTE: 이 경우, Member 데이터의 필드만 봐서 해결되는 부분이 아니고, DB까지 봐야지 처리 가능한 부분입니다.
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리
+        // 세션이 있으면 있는 세션을 반환하며, 없으면 신규 세션을 생성합니다.
+        // getSession의 변수가 true라면 없는 경우 새로 생성하여 반환하고, false인 경우, 세션이 없으면 null을 반환합니다.
+        HttpSession session = request.getSession(true);
+
+        //세션에 로그인 회원 정보를 보관합니다.
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:/";
+    }
+
 //    @PostMapping("/logout")
     public String logoutV1(HttpServletResponse response) {
         expireCookie(response, "memberId");
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
+//    @PostMapping("/logout")
     public String logoutV2(HttpServletRequest request) {
         sessionManager.expire(request);
+        return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logoutV3(HttpServletRequest request) {
+        // NOTE: 새로운 세션을 만들지 말고 null을 반환하도록 합니다. -> 로그인 하지 않은 상태로 로그아웃하는 경우
+        HttpSession session = request.getSession(false);
+        // NOTE: 로그인 되어있는 멤버가 로그아웃 하는 경우
+        if (session != null) {
+            session.invalidate();
+        }
         return "redirect:/";
     }
 
