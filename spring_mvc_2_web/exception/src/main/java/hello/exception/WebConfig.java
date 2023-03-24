84b86b22358cc96fa6881bd9855bdc2f125dfb6c
yamkant <dev.yamkim@gmail.com>
2023-03-24 11:15:51 +0900

    feat: #16 [spring_mvc_2_web] [exception] 인터셉터를 사용하여 오류페이지를 렌더링 시키는 단계에서는 인터셉터가 재호출되지 않도록 처리

--- spring_mvc_2_web/exception/src/main/java/hello/exception/WebConfig.java
+++ spring_mvc_2_web/exception/src/main/java/hello/exception/WebConfig.java
@@ -1,9 +1,11 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,7 +14,17 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    // NOTE: 서블릿은 오류가 발생했을 때도 인터셉터(혹은 필터)를, 오류 발생에 대한 페이지를 불러올 때도 인터셉터(혹은 필터)를 호출하지만,
+    //       인터셉터의 경우 dispatcherType을 세팅할 수 없는 대신, excludePathPattern을 통해 빼서 호출을 막을 수 있습니다.
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**"); // 오류 페이지 경로 추가
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
