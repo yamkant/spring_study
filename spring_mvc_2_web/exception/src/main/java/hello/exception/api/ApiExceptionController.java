a7c96418657605698d8277c8ee67685829af8183
yamkant <dev.yamkim@gmail.com>
2023-03-29 20:32:29 +0900

    feat: #18 [spring_mvc_2_web] [exception] 스프링이 제공하는 기본 exception을 사용합니다. ModelAndView 반환..

--- spring_mvc_2_web/exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ spring_mvc_2_web/exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -39,6 +40,14 @@ public class ApiExceptionController {
     @GetMapping("/api/response-status-ex2")
     public String responseStatusEx2() {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+    }
+
+    // NOTE: handleTypeMismatch 함수를 호출하고, 타입을 검사 후에 400대 오류를 반환하도록 되어있습니다. -> 여유될 때 함 쑥 훑어보기
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        // NOTE: 이는 ModelAndView를 반환하기 때문에, RestAPI랑 맞지 않습니다.
+        //       -> 스프링은 이 문제를 해결하기 위해, @ExceptionHandler를 제공합니다.
+        return "ok";
     }
 
     @Data
