d76479819f9422a6de626ef63b00459722d875ed
yamkant <dev.yamkim@gmail.com>
2023-03-31 18:08:18 +0900

    feat: #18 [spring_mvc_2_web] [typeconverter] 타입 컨버터를 위한 기본적인 사용법 추가

---
+++ spring_mvc_2_web/typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    // NOTE: /hello-v1?data=10 -> 요청 파라미터는 문자열로만 전달됩니다.
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    // NOTE:  @RequestParam-> 문자열 타입의 데이터를 알아서 변환해주는 역할을 합니다.
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+}
